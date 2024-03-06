1df988e3583b44064793bd9cc0bf9699305e15bf
Eric Liao <ericliao57@gmail.com>
2024-03-07 07:20:53 +1100

    asldkjasd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,14 +73,14 @@ const Navbar = () => {
           ? "https://i.pinimg.com/736x/01/e8/9f/01e89f7d2ddd7dbecea2e70eedab55a8.jpg"
           : "https://tetr.io/user-content/avatars/5e76f424a8a28e1c6b9e1f2f.jpg?rv=1627887094640"}
           alt="pfp"
-          className='rounded-full shadow-lg h-12 w-12 transition hover:ring hover:ring-primary-300'
+          className={`${progress >= 100 ? 'hover:ring-red-500 animate-spin' : 'hover:ring-primary-300'} rounded-full shadow-lg h-12 w-12 transition hover:ring`}
           onClick={() => { progress < 100 ? setProgress(progress => progress + 1) : window.open("https://www.cse.unsw.edu.au/~z5422235/") }} />
         <div className={`${progress > 0 ? "opacity-100" : 'opacity-0'} w-14 sm:w-64 md:w-96 bg-gray-200 rounded-full h-2 ml-3 transition-width duration-700`}>
-          <div className={`${progress >= 100 ? "bg-red-500 shadow-md shadow-red-400/40" : "bg-primary-300"} delay-100 h-2 rounded-full transition-width duration-200`} style={{ width: `${progress}%` }}></div>
+          <div className={`${progress >= 100 ? "bg-red-500 shadow-md shadow-red-400/40 animate-pulse" : "bg-primary-300"} delay-100 h-2 rounded-full transition-width duration-200`} style={{ width: `${progress}%` }}></div>
           <p className='text-xs mx-1 my-auto'>plz x{progress}</p>
           <p className='text-xs mx-1 my-auto'>{progress >= 100 ? "ggs 💀" : stop[progress % stop.length]}</p>
         </div>
-        <div className={`${progress > 0 && progress < 100 ? "opacity-100" : 'opacity-0' } hidden xl:flex space-x-1 p-1`}>
+        <div className={`${progress > 0 && progress < 100 ? "opacity-100" : 'opacity-0' } hidden xl:flex space-x-1 p-1 transition-opacity`}>
           <div className={`h-10 w-10 border rounded text-center ${ key1 ? 'bg-primary-400' : 'bg-transparent'}`}>z</div>
           <div className={`h-10 w-10 border rounded text-center ${ key2 ? 'bg-primary-400' : 'bg-transparent'}`}>x</div>
         </div>
