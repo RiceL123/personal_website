4eb41a9e351e99cd36e8252de8fcf1e190693469
Eric Liao <ericliao57@gmail.com>
2024-03-07 00:14:03 +1100

    use gif instead of vid

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -5,12 +5,18 @@ const PageNotFound = () => {
   return (
     <div>
       <Navbar />
-      <div className="grid place-items-center bg-black drop-shadow-lg rounded">
-        <div className='absolute bg-white-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100 p-2 z-10'>
+      <div className="grid place-items-center drop-shadow-lg rounded bg-black min-h-32">
+        <div className='absolute bg-white-900 rounded-md backdrop-blur border border-gray-100 p-2 z-10'>
           <h2 className='text-5xl mx-auto text-white'>404 Page Not Found</h2>
-          <p className='text-white'>here is me playing tetris (its not sped up)</p>
+          <p className='text-white'>u lost? well, here is me playing tetris (its not sped up)</p>
+          <details className='text-white text-sm' close>
+            <summary>40 Line Sprint Stats</summary>
+            <p className='ml-2'>time: 24.197s</p>
+            <p className='ml-2'>pieces per second: 4.22</p>
+            <p className='ml-2'>blocks: 102</p>
+          </details>
         </div>
-        <video src="https://raw.githubusercontent.com/RiceL123/personal_website/main/assets/sprint pb.mov" autoPlay controls loop muted className='rounded'></video>
+        <img src="https://raw.githubusercontent.com/RiceL123/personal_website/main/assets/sprint pb.gif" alt="sprint pb" />
       </div>
     </div>
   );
